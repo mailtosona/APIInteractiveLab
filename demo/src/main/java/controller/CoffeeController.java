db072a178832d4be599e3e5b9804b12830a3b3d8
mailtosona <mailtosona@ymail.com>
2022-02-22 17:36:27 +0000

    changes made

--- demo/src/main/java/controller/CoffeeController.java
+++ demo/src/main/java/controller/CoffeeController.java
@@ -9,15 +9,15 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class CoffeeController {
-    private final AtomicLong counter = new AtomicLong();
+   // private final AtomicLong counter = new AtomicLong();
 
     @GetMapping(value = "/coffeelover")
     public String coffeelover() {
         return "I like Coffee";
     }
 
-    @GetMapping("/coffee")
-    public Coffee coffee(@RequestParam(value = "name", defaultValue = "latte") String name) {
-        return new Coffee(counter.incrementAndGet(), name);
-    }
+  //  @GetMapping("/coffee")
+   // public Coffee coffee(@RequestParam(value = "name", defaultValue = "latte") String name) {
+     //   return new Coffee(counter.incrementAndGet(), name);
+    //}
 }
---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into demo(name,)
